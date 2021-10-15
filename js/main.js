9354fcd29c98f4f9ca64799655fab93181a8d98d
iamalexneumann <iamalexneumann@yandex.ru>
2021-10-15 21:51:56 +0300

    Исправляет порядок функций в файле, приведение типа для location и сортирует массив avatars случайным образом

--- js/main.js
+++ js/main.js
@@ -24,9 +24,38 @@ const getRandomNumber = (min, max) => {
 */
 const getRandomNumberFloating = (min, max, float) => (min >= 0 && min < max) ? (Math.random() * (max - min) + min).toFixed(float) : null;
 
+/**
+ * Функция, возвращающая один случайный элемент массива из переданного массива
+ * https://up.htmlacademy.ru/profession/backender/1/javascript/demos/5965#11
+ * @param {array} elements начальные элементы массива
+ * @returns Результат: случайный элемент массива
+ */
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+/**
+ * Функция, возвращающая перемешанные элементы массива из переданного массива
+ * https://learn.javascript.ru/task/shuffle
+ * @param {array} elements начальные элементы массива
+ * @returns Результат: массив с перемешанными элементами
+ */
+const shuffleArray = (elements) => elements.sort(() => Math.random() - 0.5);
+
+/**
+ * Функция, возвращающая копию части массива случайной длины из переданного массива
+ * @param {array} elements начальные элементы массива
+ * @returns Результат: массив случайной длины
+ */
+const getRandomArray = (elements) => elements.slice(0, getRandomNumber(1, elements.length-1));
+
 const ADS_COUNT = 10;
 const AVATARS_COUNT = 10;
 
+/**
+ * Функция, возвращающая строку с адресом изображения
+ * @param {*} element
+ * @param {number} index индексный номер изображения
+ * @returns Результат: строка с адресом изображения формата
+ */
 const getAvatarUrl = (element, index) => {
   index++;
   if (index >= AVATARS_COUNT) {
@@ -35,7 +64,7 @@ const getAvatarUrl = (element, index) => {
   return `img/avatars/user0${index}.png`;
 };
 
-const avatars = Array.from({length: AVATARS_COUNT}, getAvatarUrl);
+const avatars = shuffleArray(Array.from({length: AVATARS_COUNT}, getAvatarUrl));
 
 const TITLES = [
   'Апарт-Отель Депутатский',
@@ -83,28 +112,11 @@ const PHOTOS = [
 ];
 
 /**
- * Функция, возвращающая один случайный элемент массива из переданного массива
- * https://up.htmlacademy.ru/profession/backender/1/javascript/demos/5965#11
- * @param {array} elements начальные элементы массива
- * @returns Результат: случайный элемент массива
+ * Функция, возвращающая JS-объект со сгенерированными данными
+ * @param {*} element
+ * @param {number} index индексный номер объявления
+ * @returns Результат: объект со сгенерированными данными
  */
-const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
-
-/**
- * Функция, возвращающая перемешанные элементы массива из переданного массива
- * https://learn.javascript.ru/task/shuffle
- * @param {array} elements начальные элементы массива
- * @returns Результат: массив с перемешанными элементами
- */
-const shuffleArray = (elements) => elements.sort(() => Math.random() - 0.5);
-
-/**
- * Функция, возвращающая копию части массива случайной длины из переданного массива
- * @param {array} elements начальные элементы массива
- * @returns Результат: массив случайной длины
- */
-const getRandomArray = (elements) => elements.slice(0, getRandomNumber(1, elements.length-1));
-
 const createAd = (element, index) => {
   const location = {
     lat: getRandomNumberFloating(35.65000, 35.75000, 5),
@@ -129,8 +141,8 @@ const createAd = (element, index) => {
       photos: getRandomArray(shuffleArray(PHOTOS)),
     },
     location: {
-      lat: location.lat,
-      lng: location.lng,
+      lat: Number(location.lat),
+      lng: Number(location.lng),
     },
   };
 };
